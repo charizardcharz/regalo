63269e5ca1f2f2d6b8cb2e2cc5f80ed8b6717953
charizardcharz <412272+charizardcharz@users.noreply.github.com>
2024-11-26 09:47:36 -0500

    Fix Key

--- src/main/java/net/arcatanium/regalo/model/jpa/WishlistItemKey.java
+++ src/main/java/net/arcatanium/regalo/model/jpa/WishlistItemKey.java
@@ -1,7 +1,12 @@
 package net.arcatanium.regalo.model.jpa;
 
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@NoArgsConstructor
+@EqualsAndHashCode
 public class WishlistItemKey implements Serializable {
     public Integer sequenceNumber;
     public WishlistEntity wishlistEntity;
